f501c089d751079871f475ec73ca7b3b402c2f45
SA24 <sa24@Skills-Academy-29.local>
2024-11-13 18:04:02 -0500

    This is my push to github, i am pushing all my work

---
+++ python_1/Manvi.py
@@ -0,0 +1,15 @@
+# snake case 
+first_name = "Manvitha" # this is a string
+
+# camel case
+lastName = "Peela" # This 
+nickName="Manvi" # this is a string
+
+age = 14 # this is an int
+bankAccountBalance = 24.08 # this is a float
+
+print(first_name)
+print(bankAccountBalance)
+
+if bankAccountBalance > 100:
+    print("Heyyy you guys!!")
