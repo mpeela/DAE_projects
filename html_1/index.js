d8bad14882d4bbd6e1baa8ff70b71b03e6f9909e
SA24 <sa24@Skills-Academy-29.local>
2025-02-12 18:06:02 -0500

    added somethings to java

--- html_1/index.js
+++ html_1/index.js
@@ -1,2 +1,8 @@
 alert("want to learing about cats and dogs");
-varCats = "cats";
+let varCats="cats";
+let varDogs="dogs";
+const avgAgeOfDogs=12;
+const avgAgeOfCats=16;
+let avgAmountOfTimesPeopleUsedThisWebsite=0;
+console.log('how long the dogs live for!' , avgAgeOfDogs);
+console.log('how long the cats live for!' , avgAgeOfCats);
