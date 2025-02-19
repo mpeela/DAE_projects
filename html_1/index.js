b096ef63525c9213b10840f1b8fed49ad9e4f4fa
SA24 <sa24@Skills-Academy-29.local>
2025-02-19 17:48:32 -0500

    I did a little but of javaa

--- html_1/index.js
+++ html_1/index.js
@@ -1,8 +1,9 @@
-alert("want to learing about cats and dogs");
+alert("Hi you guys welcome to my website!!Do you want to lean about dogs and cats to see who you are?");
 let varCats="cats";
 let varDogs="dogs";
 const avgAgeOfDogs=12;
 const avgAgeOfCats=16;
-let avgAmountOfTimesPeopleUsedThisWebsite=0;
+let avgAmountOfTimesPeopleUsedThisWebsite=2;
 console.log('how long the dogs live for!' , avgAgeOfDogs);
 console.log('how long the cats live for!' , avgAgeOfCats);
+
