91b2968062219d193192d208426ea0a3348a5b8a
SA24 <sa24@Skills-Academy-29.local>
2025-03-03 17:55:34 -0500

    i did some java :)

--- html_1/index.js
+++ html_1/index.js
@@ -1,9 +1,10 @@
-alert("Hi you guys welcome to my website!!Do you want to lean about dogs and cats to see who you are?");
-let varCats="cats";
-let varDogs="dogs";
-const avgAgeOfDogs=12;
-const avgAgeOfCats=16;
-let avgAmountOfTimesPeopleUsedThisWebsite=2;
-console.log('how long the dogs live for!' , avgAgeOfDogs);
-console.log('how long the cats live for!' , avgAgeOfCats);
+// alert("Hi you guys welcome to my website!!Do you want to lean about dogs and cats to see who you are?");
+// let varCats="cats";
+// let varDogs="dogs";
+// const avgAgeOfDogs=12;
+// const avgAgeOfCats=16;
+// let avgAmountOfTimesPeopleUsedThisWebsite=2;
+// console.log('how long the dogs live for!' , avgAgeOfDogs);
+// console.log('how long the cats live for!' , avgAgeOfCats);
 
+alert("We got you")
--- javascript_1/index.js
+++ javascript_1/index.js
@@ -1,9 +1,22 @@
-alert("Hi you guys welcome to my website!!Do you want to lean about dogs and cats to see who you are?");
-let varCats="cats";
-let varDogs="dogs";
+// alert("This is what Manvitha wants")
+
+alert("Hi you guys welcome to my website!!Do you want to learn about dogs and cats to see who you are?");
+let cats="cats";
+let dogs="dogs";
 const avgAgeOfDogs=12;
 const avgAgeOfCats=16;
 let avgAmountOfTimesPeopleUsedThisWebsite=2;
 console.log('how long the dogs live for!' , avgAgeOfDogs);
 console.log('how long the cats live for!' , avgAgeOfCats);
+let dogEntries=1;
+let catEntries=1;
 
+console.log('Total number of linked sites: ', dogEntries + catEntries);
+
+let adoptedDogsAndCats=0
+
+if (adoptedDogsAndCats > 0 || adoptedDogsAndCats < 20) {
+    console.log('Good job, but always remember that slow and steady wins the race.')
+} else {
+    console.log('Awesome job!')
+}
